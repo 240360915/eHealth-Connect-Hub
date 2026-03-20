1ba728f380ac5a8fa42a0df9d7d10c42923d4fc7
Mthokozisi <tsambomthokozisi@gmail.com>
2026-03-21 01:38:35 +0200

    e-Health-connect-hub

---
+++ home.html/Javascript.js
@@ -0,0 +1,7 @@
+const patient=document.querySelector('.patients');
+const doctors=document.querySelector('.doctors');
+function submit(){
+    if(patient.checked){
+        document.createElement('')
+    }
+}
