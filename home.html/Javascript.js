b1421538fa15ce3d918a5c0966f4fe303e792025
Mthokozisi <tsambomthokozisi@gmail.com>
2026-03-31 23:11:36 +0200

    First upload

--- home.html/Javascript.js
+++
@@ -1,7 +0,0 @@
-const patient=document.querySelector('.patients');
-const doctors=document.querySelector('.doctors');
-function submit(){
-    if(patient.checked){
-        document.createElement('')
-    }
-}
